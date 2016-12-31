77e544a28485b051152dec6544a0755154afa6cd
thekurkure <krishanthdevaraj93@gmail.com>
2016-12-31 18:49:37 +1100

    Create initial.py

---
+++ initial.py
@@ -0,0 +1,11 @@
+# A diaglog prompt for vfire jobs.
+
+import ctypes  # An included library with Python install.
+
+if #key is pressed run dialog function /run
+
+
+# keeps running as long as hotkey was pressed
+run():
+while 
+    ctypes.windll.user32.MessageBoxW(0, "There is a job, there is work!", "ALERT", 1)
